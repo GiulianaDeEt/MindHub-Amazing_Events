3545d7ce64fe870d24e856c08ab4e2558657b9e2
Giuliana Dealbera Etchechoury <giulydealbera@gmail.com>
2023-02-24 00:32:56 -0300

    Agrego nuevos archivos correspondientes a la task2

---
+++ task2/pastEvents.js
@@ -0,0 +1,20 @@
+let htmlPast = "";
+for(let event of data.events){
+    let currentDate = new Date(data.currentDate);
+    let eventDate = new Date(event.date);
+
+    if (eventDate < currentDate) {
+        htmlPast <= `<div class="card">
+            <img src="${event.image}">
+            <div class="card-body">
+                <h5<>${event.name}</h5>
+                <p>${event.description}</p>
+                <div class="d-flex justify-content-between"> 
+                    <h6>${event.price}</h6>               
+                    <a href="./task1/details.html" class="btn btn-primary">Ver más...</a>
+                </div>
+            </div>
+        </div>`
+    }
+}
+console.log(htmlPast);
