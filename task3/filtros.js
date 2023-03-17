350c95caa6657e0e654da4aa20738bbdf178f060
Giuliana Dealbera Etchechoury <giulydealbera@gmail.com>
2023-03-17 20:24:01 -0300

    Agrego modificaciones a archivos correspondientes a la task4

--- task3/filtros.js
+++ task3/filtros.js
@@ -1,6 +1,63 @@
+// getData().then(data => {
+//     /*CREADOR DE CATEGORÍAS*/
 
-function filtroCheckbox(data){
+//     let uniqueCategories = [];
+//     let categoriesHtml = "";
+
+//     data.events.forEach(event => {
+//         if (!uniqueCategories.includes(event.category)) {
+//             uniqueCategories.push(event.category);
+//             categoriesHtml += `<label data-category="${event.category}"><input type="checkbox" value="${event.category}" name="category" class="categoria-checkbox">${event.category}</label>`;
+//         }
+//     });
+
+//     const container = document.getElementById("checkbox-container");
+//     container.innerHTML = categoriesHtml;
+
+//     /*FILTRO CATEGORIAS*/
+
+//     const checkboxes = document.querySelectorAll('.categoria-checkbox');
+//     checkboxes.forEach(checkbox => {
+//         checkbox.addEventListener('change', () => {
+//             if (checkbox.checked) {
+//                 filtroCategorias.push(checkbox.value);
+//             } else {
+//                 filtroCategorias = filtroCategorias.filter(category => category !== checkbox.value);
+//             }
+//             mostrarProductos();
+//         });
+//     });
+
+
+//     /*BUSCADOR*/
+
+//     const searchInput = document.getElementById('search-input');
+//     // const clearBtn = document.querySelector('.clear-btn');
+//     const filterBtn = document.getElementById('filterBtn');
+//     const eventsList = document.querySelector('#cardsStructure');
+//     const events = data.events;
+
+//     /*FILTRO BUSCADOR*/
+
+//     function filterEvents(searchTerm) {
+//         const filteredEvents = eventDOM.filter((event) => {
+//             return (
+//                 event.name.toLowerCase().includes(searchTerm) ||
+//                 event.description.toLowerCase().includes(searchTerm)
+//             );
+//         }); 
+//         mostrarProductos(filteredEvents);
+//     }
+
+//     // console.log(filterBtn);
+//     filterBtn.addEventListener('click', () => {
+//         const searchTerm = searchInput.value.toLowerCase();
+//         filterEvents(searchTerm);
+//     });
+// })
+getData().then(data => {
     /*CREADOR DE CATEGORÍAS*/
+
     let uniqueCategories = [];
     let categoriesHtml = "";
 
@@ -15,6 +72,7 @@ function filtroCheckbox(data){
     container.innerHTML = categoriesHtml;
 
     /*FILTRO CATEGORIAS*/
+
     const checkboxes = document.querySelectorAll('.categoria-checkbox');
     checkboxes.forEach(checkbox => {
         checkbox.addEventListener('change', () => {
@@ -23,36 +81,34 @@ function filtroCheckbox(data){
             } else {
                 filtroCategorias = filtroCategorias.filter(category => category !== checkbox.value);
             }
-            mostrarProductos();
+            const filteredEvents = data.events.filter(event => filtroCategorias.includes(event.category));
+            mostrarProductos(filteredEvents);
         });
     });
-}
 
-/*BUSCADOR*/
 
-const searchInput = document.getElementById('search-input');
-const clearBtn = document.querySelector('.clear-btn');
-const filterBtn = document.getElementById('filterBtn');
-const eventsList = document.querySelector('#cardsStructure');
-let events = [];
+    /*BUSCADOR*/
 
-/*FILTRO BUSCADOR*/
+    const searchInput = document.getElementById('search-input');
+    const filterBtn = document.getElementById('filterBtn');
+    const eventsList = document.querySelector('#cardsStructure');
 
-function filterEvents(searchTerm) {
-    const filteredEvents = eventDOM.filter((event) => {
-        return (
-            event.name.toLowerCase().includes(searchTerm) ||
-            event.description.toLowerCase().includes(searchTerm)
-        );
-    }); 
-    mostrarProductos(filteredEvents);
-}
+    /*FILTRO BUSCADOR*/
 
-console.log(filterBtn);
-filterBtn.addEventListener('click', () => {
-    const searchTerm = searchInput.value.toLowerCase();
-    filterEvents(searchTerm);
+    function filterEvents(searchTerm) {
+        const filteredEvents = data.events.filter((event) => {
+            return (
+                event.name.toLowerCase().includes(searchTerm) ||
+                event.description.toLowerCase().includes(searchTerm)
+            );
+        }); 
+        mostrarProductos(filteredEvents);
+    }
+
+    filterBtn.addEventListener('click', () => {
+        const searchTerm = searchInput.value.toLowerCase();
+        filterEvents(searchTerm);
+    });
+
+    mostrarProductos();
 });
-
-
-
